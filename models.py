2ec6ad64441cd97b6e0970697f2111f7cf275d8a
Ken Wu <kenwu1009us@gmail.com>
2023-07-07 19:14:19 -0400

    add models

---
+++ models.py
@@ -0,0 +1,56 @@
+"""
+This module defines the Item and Receipt models using Pydantic.
+
+Dependencies:
+- typing: Provides type hints.
+- pydantic: A library for data validation and serialization.
+
+Models:
+- Item (BaseModel): Represents an item in a receipt.
+  - shortDescription (str): The short description of the item.
+  - price (str): The price of the item.
+
+- Receipt (BaseModel): Represents a receipt.
+  - retailer (str): The retailer name.
+  - purchaseDate (str): The purchase date.
+  - purchaseTime (str): The purchase time.
+  - total (str): The total amount of the receipt.
+  - items (List[Item]): A list of Item objects representing the items in the receipt.
+
+Note: Pydantic's BaseModel provides automatic data validation and serialization/deserialization capabilities based on the defined field types.
+
+"""
+
+
+from typing import List
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    """
+    Represents an item in a receipt.
+
+    Attributes:
+    - shortDescription (str): The short description of the item.
+    - price (str): The price of the item.
+    """
+    shortDescription: str
+    price: str
+
+
+class Receipt(BaseModel):
+    """
+    Represents a receipt.
+
+    Attributes:
+    - retailer (str): The retailer name.
+    - purchaseDate (str): The purchase date.
+    - purchaseTime (str): The purchase time.
+    - total (str): The total amount of the receipt.
+    - items (List[Item]): A list of Item objects representing the items in the receipt.
+    """
+    retailer: str
+    purchaseDate: str
+    purchaseTime: str
+    total: str
+    items: List[Item]
