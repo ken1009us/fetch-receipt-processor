23b2356acf2e1a0fa13c031aa18e484a00cb37f2
Ken Wu <kenwu1009us@gmail.com>
2023-07-08 00:45:59 -0400

    refactor cli

--- cli.py
+++ cli.py
@@ -1,32 +1,27 @@
 """
-This module defines functions to process receipts and retrieve points using a FastAPI server.
-
-Functions:
-- process_receipt(data): Sends a POST request to the FastAPI server to process a receipt.
-- retrieve_points(receipt_id): Sends a GET request to the FastAPI server to retrieve points for a given receipt ID.
-
-Main Routine:
-- The main routine provides a menu-driven interface to interact with the functions.
-  - Option 1: Process Receipt
-    - Allows the user to manually enter receipt information or provide a path to a JSON file.
-    - Sends the receipt data to the FastAPI server for processing.
-  - Option 2: Retrieve Points
-    - Prompts the user to enter a receipt ID.
-    - Sends a request to the FastAPI server to retrieve the points and breakdown for the specified receipt ID.
-  - Option 3: Exit
-    - Terminates the program.
+This module contains a CLI program for interacting with the Fetch Receipt Processor system.
 
 Dependencies:
 - json: Provides functions for working with JSON data.
-- requests: Allows sending HTTP requests.
-- pathlib: Provides classes to work with file paths.
+- requests: Allows making HTTP requests to the FastAPI server.
+
+Functions:
+- process_receipt(data): Sends a POST request to the FastAPI server to process a receipt based on the provided data.
+- retrieve_points(receipt_id): Sends a GET request to the FastAPI server to retrieve points for a given receipt ID.
+- display_menu(): Displays the main menu options to the user.
+- process_receipt_menu(): Provides options to the user for entering receipt information.
+- get_manual_receipt_data(): Prompts the user to manually enter receipt information.
+- process_receipt_cli(): Handles the process receipt functionality in the CLI.
+- retrieve_points_cli(): Handles the retrieve points functionality in the CLI.
+- main(): Main routine for the CLI program.
+
+Note: The script assumes that the FastAPI server is running and accessible at 'http://localhost:80'. Modify the server URL as needed.
 
 """
 
-
 import json
 import requests
-
+import pyfiglet
 
 from pathlib import Path
 
@@ -42,7 +37,7 @@ def process_receipt(data):
     - "Receipt processed." and the generated receipt ID if successful.
     - "Error processing receipt." otherwise.
     """
-    response = requests.post('http://localhost:8000/receipts/process', json=data, headers={"Content-Type":"application/json"})
+    response = requests.post('http://localhost:80/receipts/process', json=data, headers={"Content-Type":"application/json"})
     if response.status_code == 200:
         receipt_id = response.json()["id"]
         print("Receipt processed.")
@@ -62,7 +57,7 @@ def retrieve_points(receipt_id):
     - The total points and breakdown if successful.
     - "Error retrieving points." otherwise.
     """
-    response = requests.get(f'http://localhost:8000/receipts/{receipt_id}/points')
+    response = requests.get(f'http://localhost:80/receipts/{receipt_id}/points')
     if response.status_code == 200:
         points = response.json()['points']
         breakdown = response.json()['breakdown']
@@ -72,58 +67,110 @@ def retrieve_points(receipt_id):
             print("   " + breakdown_item)
         print("  + ---------")
         print(f"  = {points} points")
+        print()
     else:
         print('Error retrieving points.')
 
 
-if __name__ == '__main__':
+def display_menu():
+    """
+    Displays the main menu options to the user.
+    """
+    print("1. Process Receipt")
+    print("2. Retrieve Points")
+    print("3. Exit")
+
+
+def process_receipt_menu():
+    """
+    Provides options to the user for entering receipt information.
+    """
+    print("\n--- Process Receipt ---")
+    print("1. Manually enter receipt info")
+    print("2. Load receipt info from a JSON file")
+
+
+def get_manual_receipt_data():
+    """
+    Prompts the user to manually enter receipt information.
+    """
+    print("\nEnter receipt information:")
+    retailer = input('Retailer: ')
+    purchase_date = input('Purchase Date: ')
+    purchase_time = input('Purchase Time: ')
+    total = input('Total: ')
+
+    items = []
+    num_items = int(input('Number of items: '))
+    for i in range(num_items):
+        item_description = input(f'Item {i + 1} Description: ')
+        item_price = input(f'Item {i + 1} Price: ')
+        items.append({"shortDescription": item_description, "price": item_price})
+
+    receipt_data = {
+        "retailer": retailer,
+        "purchaseDate": purchase_date,
+        "purchaseTime": purchase_time,
+        "total": total,
+        "items": items
+    }
+
+    return receipt_data
+
+
+def process_receipt_cli():
+    """
+    Handles the process receipt functionality in the CLI.
+    """
+    process_receipt_menu()
+    option = input("Enter your choice: ")
+
+    if option == '1':
+        receipt_data = get_manual_receipt_data()
+        process_receipt(receipt_data)
+    elif option == '2':
+        file_path = input("Enter the path to the JSON file: ")
+        p = Path(file_path)
+        receipt_data = json.load(open(p))
+        process_receipt(receipt_data)
+    else:
+        print("Invalid choice. Please try again.")
+
+
+def retrieve_points_cli():
+    """
+    Handles the retrieve points functionality in the CLI.
+    """
+    print("\n--- Retrieve Points ---")
+    receipt_id = input("Enter the receipt ID: ")
+    retrieve_points(receipt_id)
+
+
+def main():
+    """
+    Main routine for the CLI program.
+    """
+    f = pyfiglet.figlet_format("Fetch Rewards", font="slant", width=20)
+    print(f)
+    print("=== Fetch Receipt Processor System ===")
+
     while True:
-        print("1. Process Receipt")
-        print("2. Retrieve Points")
-        print("3. Exit")
+        display_menu()
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            receipt_data = {}
-            print("1. Manually type the info")
-            print("2. Provide the path to JSON file")
-            option = input("Enter your choice: ")
-
-            if option == '1':
-                receipt_data = {
-                    "retailer": input('Retailer: '),
-                    "purchaseDate": input('Purchase Date: '),
-                    "purchaseTime": input('Purchase Time: '),
-                    "total": input('Total: '),
-                    "items": []
-                }
-
-                num_items = int(input('Number of items: '))
-                for i in range(num_items):
-                    item = {
-                        "shortDescription": input(f'Item {i + 1} Description: '),
-                        "price": input(f'Item {i + 1} Price: ')
-                    }
-                    receipt_data["items"].append(item)
-
-            elif option == '2':
-                file_path = input("JSON file Path: ")
-                p = Path(file_path)
-                receipt_data = json.load(open(p))
-
-            process_receipt(receipt_data)
-
+            process_receipt_cli()
         elif choice == '2':
-            receipt_id = input("Enter the receipt ID: ")
-            retrieve_points(receipt_id)
-
-
+            retrieve_points_cli()
         elif choice == '3':
-            print("Thank you! Bye!")
+            print("\nThank you for using the Fetch Receipt Processor System!")
             break
-
         else:
-            print("Invalid choice. Please try again...")
+            print("Invalid choice. Please try again.")
+
+
+if __name__ == '__main__':
+    main()
 
 
 
